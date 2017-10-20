1744d50a72211fde47890ce303720d9997874672
Noble <guolei1.chen@gmail.com>
2017-10-20 16:19:33 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -4,6 +4,6 @@ import minesweeper
 print("Starting game")
 print("press 'r' for restart")
 size = int(input("Please enter the size of matrix: "))
-
+bombs = int(input("Please enter the number of bombs: "))
 position = dfs()
-minesweeper.game(size, bombs,position)
+minesweeper.game(size, bombs, position)
